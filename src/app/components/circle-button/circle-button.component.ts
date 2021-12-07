44eea4cbcd93498552c27cfd9c2ae1e4ad34a19a
viktor.hajer <viktor.hajer@evosoft.com>
2021-12-07 08:07:51 +0100

    New version

--- src/app/components/circle-button/circle-button.component.ts
+++ src/app/components/circle-button/circle-button.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-circle-button',
@@ -6,5 +6,5 @@ import {Component} from '@angular/core';
   styleUrls: ['./circle-button.component.scss']
 })
 export class CircleButtonComponent {
-
+  @Input() direction = 'left';
 }
